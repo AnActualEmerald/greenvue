e54b003ce4f4952446d830b9773466efd8c00ac1
AnActualEmerald <kevinliger13@gmail.com>
2022-04-13 01:00:14 -0400

    Start work on project showcase

---
+++ src/components/ProjectCell.vue
@@ -0,0 +1,26 @@
+<template>
+    <td>
+        <p>{{ name }}</p>
+        <p>{{ desc }}</p>
+        <p><a :href="repo" target="_blank">repo</a></p>
+    </td>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+    props: {
+        name: String,
+        repo: String,
+        desc: String,
+    },
+});
+</script>
+
+<style lang="scss" scoped>
+td {
+    padding: 2rem;
+    border: 1px solid red;
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,62 +1,75 @@
 <template>
-  <main>
-    <img
-      src="https://media2.giphy.com/media/FqdGGgugkC4Xm/giphy.gif?cid=790b76117733a46cffb567b7dc09ecbf057eaf4f53b3617b&rid=giphy.gif&ct=g"
-    />
-    <p>
-      I'm hard at work making the worst possible portfolio website for you to
-      enjoy
-    </p>
-  </main>
-  <footer>
-    <p>
-      So let me be enemy to the noose and the fading will<br />Let me be open
-      arms, tell the living world I am grateful still
-    </p>
-  </footer>
+    <main>
+        <!-- <img
+            src="https://media2.giphy.com/media/FqdGGgugkC4Xm/giphy.gif?cid=790b76117733a46cffb567b7dc09ecbf057eaf4f53b3617b&rid=giphy.gif&ct=g"
+        />
+        <p>
+            I'm hard at work making the worst possible portfolio website for you
+            to enjoy
+        </p> -->
+        <table>
+            <tr>
+                <ProjectCell name="test" repo="bet" desc="Non" />
+                <ProjectCell
+                    name="GVRH"
+                    repo="https://github.com/anactualemerald/gvrh"
+                    desc="A discord bot"
+                />
+            </tr>
+        </table>
+    </main>
+    <footer>
+        <p>
+            So let me be enemy to the noose and the fading will<br />Let me be
+            open arms, tell the living world I am grateful still
+        </p>
+    </footer>
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
+import ProjectCell from "@/components/ProjectCell.vue";
 
 export default defineComponent({
-  name: "HomeView",
+    components: {
+        ProjectCell,
+    },
 });
 </script>
 
 <style lang="scss" scoped>
 p {
-  text-align: center;
+    text-align: center;
 }
 main {
-  border: 2px solid white;
-  border-radius: 100px;
-  align-content: center;
-  padding: 50px;
-  padding-top: 100px;
-  padding-bottom: 100px;
-  margin-left: 100px;
-  margin-right: 100px;
-  p {
-    padding-top: 50pt;
-    font-size: 3rem;
-    color: lightgreen;
-    @media screen and (max-width: 700px) {
-      font-size: 1.5rem;
+    border: 2px solid white;
+    border-radius: 100px;
+    align-content: center;
+    padding: 50px;
+    padding-top: 100px;
+    padding-bottom: 100px;
+    margin-left: 100px;
+    margin-right: 100px;
+    p {
+        padding-top: 50pt;
+        font-size: 3rem;
+        color: lightgreen;
+        @media screen and (max-width: 700px) {
+            font-size: 1.5rem;
+        }
     }
-  }
-  @media screen and(max-width: 700px) {
-    img {
-      max-width: 150pt;
+    @media screen and(max-width: 700px) {
+        img {
+            max-width: 150pt;
+        }
+        margin-left: 25px;
+        margin-right: 25px;
     }
-    margin-left: 25px;
-    margin-right: 25px;
-  }
 }
 footer {
-  p {
-    color: lighten($color: black, $amount: 5%);
-    font-size: 0.5rem;
-  }
+    p {
+        color: lighten($color: black, $amount: 5%);
+        font-size: 0.5rem;
+    }
 }
 </style>
